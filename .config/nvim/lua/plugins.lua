f6fdcb2e20bd1c9873db316f3ee08c2a3737aa4e
kraftwerk28 <kefirchik3@gmail.com>
2021-08-04 00:06:01 +0300

    Update dotfiles

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -218,30 +218,7 @@ local function load(use)
 
   use {
     'sbdchd/neoformat',
-    config = function()
-      -- :Neoformat will be always ran in these filetypes
-      -- If LSP isn't capable to do nice formatting, I place that filetype below
-      vim.g.force_neoformat_filetypes = {
-        "typescript",
-        "typescriptreact",
-        "javascript",
-        "javascriptreact",
-        "lua",
-        "python",
-      }
-      for _, ft in ipairs(vim.g.force_neoformat_filetypes) do
-        vim.g['neoformat_enabled_' .. ft] = {}
-      end
-      vim.g["neoformat_enabled_python"] = {"autopep8"}
-      vim.g.neoformat_try_formatprg = 1
-      vim.g.neoformat_run_all_formatters = 1
-      -- local eslint_cfg = {
-      --   exe = "eslint",
-      --   args = {"--fix", '"%:p"'},
-      -- }
-      -- vim.g["neoformat_javascript_eslint"] = eslint_cfg
-      -- vim.g["neoformat_typescript_eslint"] = eslint_cfg
-    end,
+    config = function() require("cfg.neoformat") end,
   }
 
   use {"equalsraf/neovim-gui-shim", opt = true}
