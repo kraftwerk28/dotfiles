1a88922268999b48841d136a0a971305820fce8c
kraftwerk28 <kefirchik3@gmail.com>
2021-08-06 16:51:04 +0300

    Update dotfiles

--- .config/sway/kbd_layout_per_window.py
+++ .config/sway/kbd_layout_per_window.py
@@ -1,67 +1,67 @@
 #!/usr/bin/env python
 from i3ipc import Connection, Event, events
 
-prev_focused = -1
-windows = {}
+
 DEFAULT_LAYOUT_INDEX = 0
 
-def dump_event(event):
-    import os
-    os.system(f"notify-send {event.change}")
 
+class Handler:
+    def __init__(self, ipc):
+        self.ipc = ipc
+        self.windows = {}
+        self.prev_focused = None
+        if (focused := ipc.get_tree().find_focused()) is not None:
+            self.prev_focused = focused.id
+        ipc.on(Event.WINDOW_FOCUS, self.focus_handler)
+        ipc.on(Event.WINDOW_CLOSE, self.close_handler)
+        ipc.on(Event.WORKSPACE_INIT, self.workspace_init_handler)
 
-def focus_handler(ipc: Connection, event: events.WindowEvent):
-    dump_event(event)
-    global windows, prev_focused
+    def _get_inputs(self):
+        return [inp for inp in self.ipc.get_inputs() if inp.type == "keyboard"]
 
-    # Save current layout
-    inputs = ipc.get_inputs()
-    input_layout_data = {}
-    for input in inputs:
-        if (index := input.xkb_active_layout_index) is not None:
-            input_layout_data[input.identifier] = index
-    if prev_focused != -1:
-        windows[prev_focused] = input_layout_data
-
-    container_id = event.container.id
-    # Restore layout of the newly focused window
-    if (cur_layout_data := windows.get(container_id, None)) is not None:
-        for input_id, layout_index in cur_layout_data.items():
-            if layout_index != input_layout_data.get(input_id, None):
-                ipc.command(
-                    f"input \"{input_id}\" xkb_switch_layout "
-                    f"{layout_index}"
-                )
-    else:
+    def focus_handler(self, ipc, event: events.WindowEvent):
+        container_id = event.container.id
+        inputs = self._get_inputs()
+        input_layout_data = {}
+        # Get current layout of every input
         for input in inputs:
-            if input.xkb_active_layout_index != DEFAULT_LAYOUT_INDEX:
-                ipc.command(
-                    f"input \"{input.identifier}\" xkb_switch_layout "
-                    f"{DEFAULT_LAYOUT_INDEX}"
-                )
+            if (index := input.xkb_active_layout_index) is not None:
+                input_layout_data[input.identifier] = index
 
-    prev_focused = container_id
+        if self.prev_focused is not None and self.prev_focused != container_id:
+            self.windows[self.prev_focused] = input_layout_data
 
+        cached_layout_data = self.windows.get(container_id, None)
+        if cached_layout_data is not None:
+            # Switch layout if it doesn't match cached window settings
+            for input_id, layout_index in cached_layout_data.items():
+                if layout_index != input_layout_data.get(input_id, None):
+                    ipc.command(
+                        f"input \"{input_id}\" xkb_switch_layout "
+                        f"{layout_index}"
+                    )
+        else:
+            # If window is new, switch to default layout
+            for input in inputs:
+                if input.xkb_active_layout_index != DEFAULT_LAYOUT_INDEX:
+                    ipc.command(
+                        f"input \"{input.identifier}\" xkb_switch_layout "
+                        f"{DEFAULT_LAYOUT_INDEX}"
+                    )
+        self.prev_focused = container_id
 
-def close_handler(ipc, event):
-    windows.pop(event.container.id, None)
+    def close_handler(self, ipc, event):
+        self.windows.pop(event.container.id, None)
 
-
-def workspace_init_handler(ipc: Connection, event: events.WorkspaceEvent):
-    dump_event(event)
-    for input in ipc.get_inputs():
-        ipc.command(
-            f"input \"{input.identifier}\" xkb_switch_layout "
-            f"{DEFAULT_LAYOUT_INDEX}"
-        )
+    def workspace_init_handler(self, ipc: Connection, event: events.WorkspaceEvent):
+        for input in self._get_inputs():
+            ipc.command(
+                f"input \"{input.identifier}\" xkb_switch_layout "
+                f"{DEFAULT_LAYOUT_INDEX}"
+            )
 
 
 if __name__ == "__main__":
     ipc = Connection()
-    focused = ipc.get_tree().find_focused()
-    if focused is not None:
-        prev_focused = focused.id
-    ipc.on(Event.WINDOW_FOCUS, focus_handler)
-    ipc.on(Event.WINDOW_CLOSE, close_handler)
-    # ipc.on(Event.WORKSPACE_INIT, workspace_init_handler)
+    Handler(ipc)
     ipc.main()
