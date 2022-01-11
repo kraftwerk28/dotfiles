aa673a2658512c84495280b417d723d3b7e8ba3f
Karakurt <43828023+0Karakurt0@users.noreply.github.com>
2022-01-11 14:00:18 +0200

    That should fix it

--- .config/sway/screenshot.sh
+++ .config/sway/screenshot.sh
@@ -6,21 +6,19 @@ fi
 img_name="$HOME/Pictures/grimshot-$(date +"%x-%X").png"
 fname=$(mktemp -u --suffix .png)
 
+copy="wl-copy -n < \"$fname\";\
+      notify-send \"Screenshot copied to clipboard\""
+save="mv \"$fname\" \"$img_name\";\
+      notify-send \"Screenshot saved to $img_name\""
+
 if grimshot save "$WHAT" "$fname"; then
-     swaynag \
-        -t warning \
+     swaynag 				     \
+        -t warning 			     \
         -m "What to do with the screenshot?" \
-        -y overlay \
-        -Z Copy "\
-            wl-copy -n < "$fname";\
-            notify-send "Screenshot copied to clipboard";\
-            "\
-        -Z Save " \
-            name="$(img_name)";\
-            mv "$fname" "$name";\
-            notify-send "Screenshot saved to $name";\
-            "\
-        -Z Edit "swappy -f "$fname"" \
+        -y overlay 			     \
+        -Z Copy "$copy" 		     \
+        -Z Save "$save" 		     \
+        -Z Edit "swappy -f "$fname"" 	     \
         --no-dock
 else
         exit 0
