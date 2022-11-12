395d68ababb272403b28e4dfcc3f23f25cafc2df
kraftwerk28 <kefirchik3@gmail.com>
2022-11-12 18:35:16 +0200

    Update dotfiles

---
+++ .config/nvim/lua/kraftwerk28/plugins/fugitive.lua
@@ -0,0 +1,15 @@
+m:withopt({ silent = true }, function()
+  m("n", "<Leader>gs", "<Cmd>vert Git<CR>")
+  m({ "n", "v" }, "<Leader>gb", ":GBrowse<CR>")
+  -- Merge conflicts
+  m("n", "<Leader>gm", "<Cmd>Gdiffsplit!<CR>")
+  m("n", "<Leader>gh", "<Cmd>diffget //2<CR>")
+  m("n", "<Leader>gl", "<Cmd>diffget //3<CR>")
+end)
+
+au("FileType", {
+  pattern = "fugitive",
+  callback = function()
+    m("n", "<Leader>gp", "<Cmd>Git push origin HEAD<CR>", { buffer = true })
+  end,
+})
---
+++ .config/nvim/lua/kraftwerk28/plugins/textcase.lua
@@ -0,0 +1,2 @@
+require("telescope").load_extension("textcase")
+m("v", "<Leader>cc", "<Cmd>TextCaseOpenTelescope<CR>")
