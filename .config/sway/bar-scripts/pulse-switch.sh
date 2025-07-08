856bc1f5d749e3a2d49e319d0a977ed3ca16b5aa
kraftwerk28 <kefirchik3@gmail.com>
2025-07-08 17:51:47 +0300

    Update dotfiles

--- .config/sway/bar-scripts/pulse-switch.sh
+++ .config/sway/bar-scripts/pulse-switch.sh
@@ -2,15 +2,15 @@
 set -e
 
 kind=$1
-if [[ $kind != sink && $kind != source ]]; then
-	echo "Specify either sink or souce" >&2
+if [[ $kind != "sink" && $kind != "source" ]]; then
+	echo "Specify either sink or source" >&2
 	exit 1
 fi
 
-next_sink="$(pactl -f json list short "${kind}s" \
+next="$(pactl -f json list short "${kind}s" \
 	| jq -e -r '
 		last(to_entries[] | select(.value.state == "RUNNING")).key as $active_idx
 		| ($active_idx + 1) % length as $next_idx
 		| .[$next_idx].name
 	')"
-pactl "set-default-${kind}" "$next_sink"
+pactl "set-default-${kind}" "$next"
